fba51678424cf11be7388dee1094e8eeb35627f4
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-10 07:51:09 +0530

    Update authAPI.js

--- Frontend/src/features/auth/authAPI.js
+++ Frontend/src/features/auth/authAPI.js
@@ -1,6 +1,6 @@
 // src/features/auth/authAPI.js
 
-const API_BASE_URL = "https://company-management-5yta.onrender.com" 
+const API_BASE_URL = "https://company-management-backend-irc6.onrender.com" 
 
 const loginUserAPI = async (credentials) => {
   try {
