fc23c6a4f42c666aa82947abb11bf23aea31efc4
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-10 07:51:57 +0530

    Update companyAPI.js

--- Frontend/src/features/company/companyAPI.js
+++ Frontend/src/features/company/companyAPI.js
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = "https://company-management-5yta.onrender.com"
+const API_BASE_URL = "https://company-management-backend-irc6.onrender.com"
 
 const createCompany = async (data) => {
   try {
