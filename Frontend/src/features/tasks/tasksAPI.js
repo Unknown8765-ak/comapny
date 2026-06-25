377250d3de9d625d909ec20f65c5bf3c6dbbc3a7
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-25 19:40:56 +0530

    Update API_BASE_URL to include version and endpoint

--- Frontend/src/features/tasks/tasksAPI.js
+++ Frontend/src/features/tasks/tasksAPI.js
@@ -1,6 +1,6 @@
 // src/features/tasks/taskAPI.js
 
-const API_BASE_URL = "https://company-management-backend-irc6.onrender.com"
+const API_BASE_URL = "https://company-management-backend-irc6.onrender.com/api/v1/tasks"
 
 
 
