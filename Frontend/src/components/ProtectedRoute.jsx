12fd0580f778d3dfea9b6405abd5cbb342a8ebf7
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 20:53:16 +0530

    Update ProtectedRoute.jsx

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -6,9 +6,7 @@ function ProtectedRoute() {
     console.log("AUTH STATE 👉", status, "LOADING 👉", loading);
 
  if (loading) return( <div className="flex justify-center items-center py-10">
-    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin">
-    Checking authentication...
-    </div>
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
   </div>
     )
 
