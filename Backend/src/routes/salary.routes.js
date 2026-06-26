064c5db4286d4c1f5dfd4896894efee4483fb927
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 12:40:43 +0530

    Change route from '/' to '/all' for getAllSalaries

--- Backend/src/routes/salary.routes.js
+++ Backend/src/routes/salary.routes.js
@@ -12,7 +12,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 router.post("/generate", verifyJWT,generateSalary);
 router.get("/me", verifyJWT, getMySalary);
-router.get("/", verifyJWT,getAllSalaries);
+router.get("/all", verifyJWT,getAllSalaries);
 router.patch("/pay/:id",verifyJWT, markSalaryPaid);
 router.get("/employee/:id",verifyJWT,getEmployeeSalaryDetails);
 
