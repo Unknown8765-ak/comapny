0b2d369c74019a566347c641997830ce5d1dbddc
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 20:51:28 +0530

    Update ProtectedRoute.jsx

--- Frontend/src/components/ProtectedRoute.jsx
+++ Frontend/src/components/ProtectedRoute.jsx
@@ -5,9 +5,10 @@ function ProtectedRoute() {
   const { status,loading } = useSelector((state) => state.auth)
     console.log("AUTH STATE 👉", status, "LOADING 👉", loading);
 
-  if (loading) {
-    return <div>Checking authentication...</div>;
-  }
+ if (loading) return( <div className="flex justify-center items-center py-10">
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+  </div>
+    )
 
   if (!status) {
     return <Navigate to="/login" replace />
