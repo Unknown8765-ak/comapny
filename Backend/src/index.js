a9a10968b9a64bad6d19decb03109b52a58d0c0a
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 12:25:23 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,9 +1,19 @@
 import dbConnection from "./db/index.js";
 import dotenv from "dotenv";
 import { app } from "./app.js"
+import Razorpay from "razorpay"
 dotenv.config({
     path : "./.env"
 })
+
+export const instance = new Razorpay({
+  key_id: process.env.Test_API_Key,
+  key_secret: process.env.Test_Key_Secret ,
+
+});
+
+
+
 
 const PORT = process.env.PORT || 8000;
 
