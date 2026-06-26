3332501f17c6ffe54a8d95b365ccba7803f21576
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 11:27:58 +0530

    Update AdminDashboard.jsx

--- Frontend/src/pages/AdminDashboard.jsx
+++ Frontend/src/pages/AdminDashboard.jsx
@@ -282,7 +282,10 @@ useEffect(() => {
   
   const renderContent = () => {
 
-    if (loading) return <p>Loading...</p>
+    if (loading) return( <div className="flex justify-center items-center py-10">
+    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+  </div>
+    )
 
     if (active === "dashboard") {
       return (
