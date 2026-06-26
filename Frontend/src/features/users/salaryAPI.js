4f805f720125b4cba30505e854e8da6e682000a9
Amit Maurya <unknownstudent8765@gmail.com>
2026-06-26 12:34:50 +0530

    Update salaryAPI.js

--- Frontend/src/features/users/salaryAPI.js
+++ Frontend/src/features/users/salaryAPI.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://company-management-backend-irc6.onrender.com"
+const API_BASE_URL = "https://company-management-backend-irc6.onrender.com/api/v1/payment"
 
 const generateSalaryAPI = async (data) => {
   try {
